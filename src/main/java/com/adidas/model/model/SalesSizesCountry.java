d491bc512bca21c9809d25edb45eafd3585a8b71
Antonio Signore <antonio.signore@gmail.com>
2018-05-29 16:59:00 +0200

    sizs smoke test

---
+++ src/main/java/com/adidas/model/model/SalesSizesCountry.java
@@ -0,0 +1,16 @@
+package com.adidas.model.model;
+
+import com.fasterxml.jackson.annotation.JsonInclude;
+import lombok.Data;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Data
+@JsonInclude(JsonInclude.Include.NON_NULL)
+public class SalesSizesCountry {
+
+    private String scaleCode;
+    private List<SalesSizeValue> salesSizes = new ArrayList<>();
+
+}
